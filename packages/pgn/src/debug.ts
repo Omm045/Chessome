c1fd2db7d654764548e0b0c7f0b21f9ce544eb21
Omm Prakash Panda <haste@Omms-MacBook-Air.local>
2026-06-30 12:19:38 +0530

    fix: resolve ts type errors in pgn debug

--- packages/pgn/src/debug.ts
+++ packages/pgn/src/debug.ts
@@ -1,10 +1,9 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
-// @ts-nocheck
 import { parse } from '@mliebelt/pgn-parser';
 
 console.log("=== ANNOTATIONS ===");
 const pgn1 = `1. e4 {[%clk 1:00:00]} e5 {[%clk 0:59:58] [%eval -0.5]}`;
-console.dir(parse(pgn1, { startRule: 'games' })[0].moves, { depth: null });
+console.dir((parse(pgn1, { startRule: 'games' }) as any)[0].moves, { depth: null });
 
 console.log("=== OFFICIAL TAGS ===");
 const pgn2 = `[Event "FIDE World Cup 2017"]
@@ -20,4 +19,4 @@ const pgn2 = `[Event "FIDE World Cup 2017"]
 [ECO "C55"]
 
 1. e4 0-1`;
-console.dir(parse(pgn2, { startRule: 'games' })[0].tags, { depth: null });
+console.dir((parse(pgn2, { startRule: 'games' }) as any)[0].tags, { depth: null });
